47c6de547e85e532339d9280f25294fb9ccac309
Justin E. Samuels <js1599@msstate.edu>
2017-03-21 02:02:40 -0500

    Menu now targets correct button clicked. Working on Redux store for functionality

--- app/components/Supervisor/index.js
+++ app/components/Supervisor/index.js
@@ -4,18 +4,25 @@ export default class Supervisor extends React.PureComponent { // eslint-disable-
      constructor(props) {
       super(props);
       //this.x = this.x.bind(this);
-      this.sw = this.sw.bind(this);
+      this.menu = this.menu.bind(this);
       //this.state = {x: '' };
   }
   
-  sw(e){
-    console.log(e.target);
+  menu(e){
+    //console.log(e.target.innerHTML );
+    let menuSelect = e.target.innerHTML;
+    var menu = {
+      'Add Student': 'Add Student'
+    }
+    return 'The drink I chose was ' + (drinks[type] || drinks['default']);
+ }
+
   }
    
   render() {
     return (
-      <div >
-        <h2 onClick = { this.sw } value = {1}>Add Student</h2>
+      <div onClick = { this.menu } >
+        <h2>Add Student</h2>
         <h2>Remove Student</h2>
         <h2>Clock Student In</h2>
         <h2>Clock Student Out</h2>
