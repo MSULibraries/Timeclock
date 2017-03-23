56ca0b9128e580a4e8d9f6416439ad575a32a305
Justin E. Samuels <js1599@msstate.edu>
2017-03-23 17:20:34 -0500

    Dynamic rendering now working. Working on Adding the views

--- app/components/AddStudent/index.js
+++ app/components/AddStudent/index.js
@@ -5,41 +5,41 @@ export default class AddStudent extends React.PureComponent { // eslint-disable-
       super(props);
       //this.x = this.x.bind(this);
       //this.state = {x: '' }; 
-         this.state = {div: false }; 
-     }
-     menu(){
-       var menu = {
-       'Add Student': () => this.setState({div:true}),
-       'Remove Student': () => this.student(),
-       'Clock Student In': () => this.clock(),
-       'Clock Student Out': () => this.clock(),
-       'Print Timesheet': () =>  this.print()
-       }
-       return ( menu[this.props.option] || menu['default'] ) () ;
-     }
-     student(){
-     }
-     clock(){
-     console.log('clock method');
-     }
-     print(){
-     console.log('print method');
-     }
-     componentWillReceiveProps(){
-       this.menu()
+         this.state = {option: 'add' }; 
      }
  
   render() {
-   //Trying to figure out how to dynamic render
+   if(this.props.option == "Add"){
     return (
-      <div ref = 'div'>
-       {this.state.div}
-      </div>
+      <div>
+       <label>Net ID: <input type = "text" /> </label><br /> 
+       <label>First Name: <input type = "text" /> </label><br />
+       <label>Last Name: <input type = "text" /> </label><br />
+       <label>Employee Type: <input type = "radio" name = "StudentType" value = "UG" /></label>Undergrad &nbsp; 
+                             <input type = "radio" name = "StudentType" value = "GR" />Grad<br />
+       <label>Sex: <select>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+      </label><br />
+      <label>Race: <select>
+                    <option value="AA">African American</option>
+                    <option value="Cauc">Caucsian</option>
+                    <option value="Asn">Asian</option>
+                    <option value="Hisp">Hispanic</option>
+                    <option value="Oth">Other</option>
+                  </select>
+      </label>
+      
+     </div>
     );
+   }
+   else{
+     return(
+       <div><label>Enter Net ID: <input type = "text" /> </label><br /></div>
+     )
+   }
    
   }
 }
 
-AddStudent.defaultProps = {
-         option: 'default'
-       };
--- app/components/Supervisor/index.js
+++ app/components/Supervisor/index.js
@@ -1,48 +1,39 @@
 import React from 'react';
 import { Link, browserHistory } from 'react-router';
-import AddStudent from '../AddStudent';
+import StudentControl from '../AddStudent';
 
 export default class Supervisor extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
      constructor(props) {
       super(props);
-      //this.x = this.x.bind(this);
       this.menu = this.menu.bind(this);
-      //this.state = {x: '' };
-      this.state = {flag: false, option: '' };
+      this.state = { option: '' };
   }
   
   menu(e){
     //console.log(e.target.innerHTML );
-    this.setState({ flag: false });
     let menuSelect = e.target.innerHTML;
     var menu = {
-      'Add Student': () =>  this.setState({ option: 'Add Student' }),
-      'Remove Student': () => this.setState({ option: 'Remove Student' }),
-      'Clock Student In': () => this.setState({ option: 'Clock Student In' }),
-      'Clock Student Out': () => this.setState({ option: 'Clock Student Out' }),
-      'Print Timesheet': () => this.setState({ option: 'Print Timesheet' }),
+      'Add Student': () =>  this.setState({ option: <StudentControl option = "Add" /> }),
+      'View Students': () => this.setState({ option: <StudentControl option = "Remove" /> }),
+      /*
+      'Clock Student In': () => this.setState({ option: <Clock option = "in" /> }),
+      'Clock Student Out': () => this.setState({ option: <Clock option = "out"/> }),
+      'Print Timesheet': () => this.setState({ option: <Print /> }),
+      */
       'default': () => console.log('def')
      }
-    this.setState({ flag: true })
      return ( menu[menuSelect] || menu['default'] ) () ; 
   }
 
   render() {
-    
     return (
       <div>
         <div onClick = { this.menu } >
           <h2>Add Student</h2>
-          <h2>Remove Student</h2>
-          <h2>Clock Student In</h2>
-          <h2>Clock Student Out</h2>
+          <h2>View Students</h2>
           <h2>Print Timesheet</h2>
         </div>
-        { this.state.flag ?  
-         <div> 
-           <AddStudent option = {this.state.option} /> 
-        </div>  : ''
-       }
+        {this.state.option}
       </div>
     );
   }
