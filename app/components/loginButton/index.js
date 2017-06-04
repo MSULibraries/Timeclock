2929d6f2ec2ca88ef3a1958f4ce2ca939ca360db
Justin E. Samuels <js1599@users.noreply.github.com>
2017-06-04 18:24:25 -0500

    Revert back

--- app/components/loginButton/index.js
+++ app/components/loginButton/index.js
@@ -4,7 +4,7 @@ const LoginButton = styled.button`
     clear: both;
     width: 100%;
     display: block;
-    background: yellow;
+    background: #00aadc;
     border-color: #0087be;
     color: white;
     border-style: solid;
