351a121c0ee4c06eb73a6a03932be7409fb0b1d4
Brooks Becton <broabect@ut.utm.edu>
2017-06-05 14:29:03 -0500

    Changed table name for AddStudent Actions

--- app/components/AddStudent/actions/index.js
+++ app/components/AddStudent/actions/index.js
@@ -6,22 +6,23 @@ export function FormAction(action, state) {
     return {
       type: "CHECK-STUDENT-STATUS",
       NetID: state.NetID,
-      query: "SELECT NetID FROM student_hours WHERE NetID='" + state.NetID + "'"
+      query: "SELECT NetID FROM Users WHERE NetID='" + state.NetID + "'"
     }
   }
   else if (action == 'SUBMIT-STUDENT') {
+    console.log("INSERT INTO Users (NetID, 9Digit, FirstName, LastName, EmployeeType, Phone, Addr, Sex, Race, Department1, Department2, Department3, Department4, WS ) VALUES('" + state.NetID + "','" + state.NINEdigit + "','" + state.firstName + "','" + state.lastName + "','" + state.employeeType + "','" + state.Phone + "','" + state.Addr + "','" + state.sex + "','" + state.race + "','" + state.dept1 + "','" + state.dept2 + "','" + state.dept3 + "','" + state.dept4 + "','" + state.WS + "') ");
     if (state.status === "true") {
       return {
         type: "SUBMIT-STUDENT",
         NetID: state.NetID,
-        query: "INSERT INTO student_hours (NetID, 9Digit, FirstName, LastName, EmployeeType, Phone, Addr, Sex, Race, Department1, Department2, Department3, Department4, WS ) VALUES('" + state.NetID + "','" + state.NINEdigit + "','" + state.firstName + "','" + state.lastName + "','" + state.employeeType + "','" + state.Phone + "','" + state.Addr + "','" + state.sex + "','" + state.race + "','" + state.dept1 + "','" + state.dept2 + "','" + state.dept3 + "','" + state.dept4 + "','" + state.WS + "') "
+        query: "INSERT INTO Users (NetID, 9Digit, FirstName, LastName, EmployeeType, Phone, Addr, Sex, Race, Department1, Department2, Department3, Department4, WS ) VALUES('" + state.NetID + "','" + state.NINEdigit + "','" + state.firstName + "','" + state.lastName + "','" + state.employeeType + "','" + state.Phone + "','" + state.Addr + "','" + state.sex + "','" + state.race + "','" + state.dept1 + "','" + state.dept2 + "','" + state.dept3 + "','" + state.dept4 + "','" + state.WS + "') "
       }
     }
     else {
       return {
         type: "SUBMIT-STUDENT",
         NetID: state.NetID,
-        query: "UPDATE student_hours SET FirstName ='" + state.firstName + "', 9Digit ='" + state.NINEdigit + "', LastName ='" + state.lastName + "', EmployeeType ='" + state.employeeType + "', Sex ='" + state.sex + "', Race ='" + state.race + "', Department1 ='" + state.dept1 + "', Department2 ='" + state.dept2 + "', Department3 ='" + state.dept3 + "', Department4 ='" + state.dept4 + "' WHERE NetID ='" + state.NetID + "'"
+        query: "UPDATE Users SET FirstName ='" + state.firstName + "', 9Digit ='" + state.NINEdigit + "', LastName ='" + state.lastName + "', EmployeeType ='" + state.employeeType + "', Sex ='" + state.sex + "', Race ='" + state.race + "', Department1 ='" + state.dept1 + "', Department2 ='" + state.dept2 + "', Department3 ='" + state.dept3 + "', Department4 ='" + state.dept4 + "' WHERE NetID ='" + state.NetID + "'"
       }
     }
   }
