af95f54adb15d103ceec3070f9198ffa73289377
Justin E. Samuels <js1599@msstate.edu>
2017-06-05 20:37:33 -0500

    minor fixes to update homepage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -27,116 +27,116 @@ import { createStructuredSelector } from 'reselect';
 var DSF = "DSF";
 
 class Home extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { user: '', dept: '' };
-    this.success = this.success.bind(this);
-    this.updateDept = this.updateDept.bind(this);
-  }
-
-  //Verifies the JWT (token) before the component (login screen) is loaded. 
-  //If the token is valid, the user is allowed into the timeclock
-  //If the token is invalid, the user is logged out of CAS and returned to the main screen
-  componentWillMount() {
-    var url = window.location.search;
-    url = url.replace("?", '');
-    if (url != '') {
-      this.setState({ token: url })
-      fetch('/verify', {
-        method: 'POST',
-        body: url
-      })
-        .then((result) => {
-          return result.json();
-        })
-        .then((response) => {
-          this.props.onChangeUser('CHECK-USER', response.user);
-          this.setState({ user: response.user });
-        })
-        .catch(function (error) {
-          window.location = "./logout";
-          // console.log(error);
-        });
+    constructor(props) {
+        super(props);
+        this.state = { user: '', dept: '' };
+        this.success = this.success.bind(this);
+        this.updateDept = this.updateDept.bind(this);
     }
-    else {
-      this.setState({ user: null });
-      //window.location = './logout';
+
+    //Verifies the JWT (token) before the component (login screen) is loaded. 
+    //If the token is valid, the user is allowed into the timeclock
+    //If the token is invalid, the user is logged out of CAS and returned to the main screen
+    componentWillMount() {
+        var url = window.location.search;
+        url = url.replace("?", '');
+        if (url != '') {
+            this.setState({ token: url })
+            fetch('/verify', {
+                method: 'POST',
+                body: url
+            })
+                .then((result) => {
+                    return result.json();
+                })
+                .then((response) => {
+                    this.props.onChangeUser('CHECK-USER', response.user);
+                    this.setState({ user: response.user });
+                })
+                .catch(function(error) {
+                    window.location = "./logout";
+                    // console.log(error);
+                });
+        }
+        else {
+            this.setState({ user: null });
+            //window.location = './logout';
+        }
     }
-  }
 
-  //Updates the department based off the user's selection from the dropdown box.
-  //If the Dean Special Fund flag is true, and set to the same as the department
-  //That student is then logged in under Dean Special Fund AND the department
-  updateDept(event) {
-    this.props.onChangeUser('SET-MAC', this.state.user, event.target.value);
-    this.props.userStatus.DSF == event.target.value ? this.setState({ dept: DSF }) : this.setState({ dept: event.target.value });
-  }
+    //Updates the department based off the user's selection from the dropdown box.
+    //If the Dean Special Fund flag is true, and set to the same as the department
+    //That student is then logged in under Dean Special Fund AND the department
+    updateDept(event) {
+        this.props.onChangeUser('SET-MAC', this.state.user, event.target.value);
+        this.props.userStatus.DSF == event.target.value ? this.setState({ dept: DSF }) : this.setState({ dept: event.target.value });
+    }
 
-  //On successful clock-in or out, the student is logged out of CAS after 2 seconds (2000 Miliseconds)
-  success() {
-    setTimeout(() => window.location = './logout', 2000)
-  }
+    //On successful clock-in or out, the student is logged out of CAS after 2 seconds (2000 Miliseconds)
+    success() {
+        //setTimeout(() => window.location = './logout', 2000)
+    }
 
-  render() {
-    return (
-      <Wrapper>
-        <Grid.Container id="container">
-          <Grid.Row>
-            <Grid.Col md={12} offset={{ md: 7 }}>
-              <LoginBox>
-                <h1>MSU Library <br /> Time Clock </h1>
-                {this.props.userStatus.Role != "student" && this.state.user != null ?
-                  <LoginButton onClick={() => window.location = "/dashboard?" + this.state.token} > Dashboard </LoginButton>
-                  : ''}
+    render() {
+        return (
+            <Wrapper>
+                <Grid.Container id="container">
+                    <Grid.Row>
+                        <Grid.Col md={12} offset={{ md: 7 }}>
+                            <LoginBox>
+                                <h1>MSU Library <br /> Time Clock </h1>
+                                {this.props.userStatus.Role != "student" && this.state.user != null ?
+                                    <LoginButton onClick={() => window.location = "/dashboard?" + this.state.token} > Dashboard </LoginButton>
+                                    : ''}
 
-                {(this.props.userStatus.Role == "student") ?
-                  <div>
-                    <select onChange={this.updateDept}>
-                      <option value="22" >Select Your Department</option>
-                      <option value="4C-72-B9-55-CD-C3" >{this.props.userStatus.Department1}</option>
-                      {this.props.userStatus.Department2 != '' ? <option value="4C-72-B9-55-CD-1111" >{this.props.userStatus.Department2}</option> : ''}
-                      {this.props.userStatus.Department3 != '' ? <option value="4C-72-B9-55-CD-C3" >{this.props.userStatus.Department3}</option> : ''}
-                      {this.props.userStatus.Department4 != '' ? <option value="4C-72-B9-55-CD-C3" >{this.props.userStatus.Department4}</option> : ''}
-                    </select> <br /><br />
-                  </div>
-                  : ''}
+                                {(this.props.userStatus.Role == "student" && this.props.userStatus.UserLoggedIn != 1) ?
+                                    <div>
+                                        <select onChange={this.updateDept}>
+                                            <option value="22" >Select Your Department</option>
+                                            <option value={this.props.userStatus.Department1} >{this.props.userStatus.Department1}</option>
+                                            {this.props.userStatus.Department2 != '' ? <option value={this.props.userStatus.Department2} >{this.props.userStatus.Department2}</option> : ''}
+                                            {this.props.userStatus.Department3 != '' ? <option value={this.props.userStatus.Department3} >{this.props.userStatus.Department3}</option> : ''}
+                                            {this.props.userStatus.Department4 != '' ? <option value={this.props.userStatus.Department4} >{this.props.userStatus.Department4}</option> : ''}
+                                        </select> <br /><br />
+                                    </div>
+                                    : ''}
 
-                {(this.props.userStatus.UserLoggedIn == 0 && this.props.userStatus.Role == "student") ?
-                  <LoginButton disabled={!this.state.dept} onClick={() => this.props.onChangeUser('USER-REQUEST-LOGIN', this.state.user, this.state.dept)} > Clock In </LoginButton>
-                  : ''}
+                                {(this.props.userStatus.UserLoggedIn == 0 && this.props.userStatus.Role == "student") ?
+                                    <LoginButton disabled={!this.state.dept} onClick={() => this.props.onChangeUser('USER-REQUEST-LOGIN', this.state.user, this.state.dept)} > Clock In </LoginButton>
+                                    : ''}
 
-                {(this.props.userStatus.UserLoggedIn == 1 && this.props.userStatus.Role == "student") ?
-                  <LoginButton onClick={() => this.props.onChangeUser('USER-REQUEST-LOGOUT', this.state.user, this.state.dept)} > Clock Out </LoginButton>
-                  : ''} <br />
+                                {(this.props.userStatus.UserLoggedIn == 1 && this.props.userStatus.Role == "student") ?
+                                    <LoginButton onClick={() => this.props.onChangeUser('USER-REQUEST-LOGOUT', this.state.user, this.state.dept)} > Clock Out </LoginButton>
+                                    : ''} <br />
 
-                {this.state.user != null ?
-                  < LogOutButton onClick={() => window.location = "/logout"} >Logout</LogOutButton>
-                  : < LoginButton onClick={() => window.location = "/cas"} > Login </LoginButton>
-                }
-                {this.props.response ? this.success() : ''}
-                {this.props.response ? <h2>Clocked in. Redirecting… </h2> : ''}
-              </LoginBox>
-            </Grid.Col>
-          </Grid.Row>
-        </Grid.Container>
-      </Wrapper>
-    );
-  }
+                                {this.state.user != null ?
+                                    < LogOutButton onClick={() => window.location = "/logout"} >Logout</LogOutButton>
+                                    : < LoginButton onClick={() => window.location = "/cas"} > Login </LoginButton>
+                                }
+                                {this.props.response ? this.success() : ''}
+                                {this.props.response ? <h2>Clocked in. Redirecting… </h2> : ''}
+                            </LoginBox>
+                        </Grid.Col>
+                    </Grid.Row>
+                </Grid.Container>
+            </Wrapper>
+        );
+    }
 }
 
 //Redux method to allow the props to have access to the Redux global store
 //With the least minimal state representation possible through Reselect library
 const mapStateToProps = createStructuredSelector({
-  user: getUser(),
-  response: getRes(),
-  userStatus: getStatusOnUser()
+    user: getUser(),
+    response: getRes(),
+    userStatus: getStatusOnUser()
 });
 
 //Redux method to bind the actions created in the component to a dispatch
 export function mapDispatchToProps(dispatch) {
-  return {
-    onChangeUser: (evt, name, mac) => dispatch(LogAction(evt, name, mac))
-  };
+    return {
+        onChangeUser: (evt, name, mac) => dispatch(LogAction(evt, name, mac))
+    };
 }
 
 //Exports class to be used, wrapped in the Redux Higher Order Component (HOC) connect()
